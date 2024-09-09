cc15af32ff84c20087c55047d8d2e91f18207941
vermino-uz <shukurullayev.elbek@icloud.com>
2024-09-10 03:15:55 +0500

    message

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -55,6 +55,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->plugin(\TomatoPHP\FilamentPWA\FilamentPWAPlugin::make());
     }
 }
