9c417c860eacf5b99c3f80b53d57241492fce138
vermino-uz <shukurullayev.elbek@icloud.com>
2024-09-12 02:08:54 +0500

    message

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -19,14 +19,14 @@ class TelegramController extends Controller
                 'chat_id' => $chat_id,
                 'photo' => InputFile::create('https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'),
                 'caption' => 'Welcome to our bot!',
-                'reply_markup' => [
+                'reply_markup' => json_encode([
                     'inline_keyboard' => [
                         [
                             'text' => 'Google',
                             'url' => 'https://www.google.com',
                         ],
                     ],
-                ],
+                ]), 
             ]);
         }
        
