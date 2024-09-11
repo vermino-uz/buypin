e961d203207eb353fe2c4ef00464fea5c250f55e
vermino-uz <shukurullayev.elbek@icloud.com>
2024-09-11 23:17:26 +0500

    message

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -12,7 +12,8 @@ use Illuminate\Database\Eloquent\Model;
  *     description="Banner model",
  *     @OA\Property(property="id", type="integer", description="ID of the banner"),
  *     @OA\Property(property="image", type="string", description="Image of the banner"),
- *     @OA\Property(property="text", type="string", description="Text of the banner"),
+ *     @OA\Property(property="name", type="string", description="Name of the banner"),
+ *     @OA\Property(property="description", type="string", description="Description of the banner"),
  *     @OA\Property(property="url", type="string", description="URL of the banner"),
  * )
  */
@@ -22,7 +23,8 @@ class Banner extends Model
 
     protected $fillable = [
         'image',
-        'text',
+        'name',
+        'description',
         'url',
     ];
 }
