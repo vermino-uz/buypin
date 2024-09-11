bae9db28f2f8e6afdde4dedecb3f23e49ee1403a
vermino-uz <shukurullayev.elbek@icloud.com>
2024-09-12 01:28:17 +0500

    message

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -1,14 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Telegram\Bot\Laravel\Facades\Telegram;
 use Illuminate\Http\Request;
-use Telegram\Bot\Api;
 class TelegramController extends Controller
 {
     public function webhook(Request $request)
     {
-        $telegram = new Api(env('TELEGRAM_BOT_TOKEN'));
-        $telegram->setWebhook(['url' => env('TELEGRAM_WEBHOOK_URL')]);
+        $bot = Telegram::bot('mybot');
+        $update = $bot->getWebhookUpdate();
+        $message = $update->getMessage();
+        $chat = $message->getChat();
+        $chatId = $chat->getId();
+
+        if ($message->getText() === '/start') {
+            // Path to your photo
+
+            // Send the photo
+            $bot->sendPhoto([
+                'chat_id' => $chatId,
+                'photo' => "https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg",
+                'caption' => 'Welcome to our bot! Here\'s a greeting photo.',
+            ]);
+        }
     }
 }
