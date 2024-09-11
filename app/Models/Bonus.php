bc6146dd08b6b2359e9de4248744f5ee4f0d1802
vermino-uz <shukurullayev.elbek@icloud.com>
2024-09-11 20:44:18 +0500

    hadhawhd

--- app/Models/Bonus.php
+++ app/Models/Bonus.php
@@ -5,6 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *     schema="Bonus",
+ *     type="object",
+ *     title="Bonus",
+ *     description="Bonus model",
+ *     @OA\Property(property="id", type="integer", description="ID of the bonus"),
+ *     @OA\Property(property="promo", type="string", description="Promo code of the bonus"),
+ *     @OA\Property(property="count_to_use", type="integer", description="Count to use of the bonus"),
+ *     @OA\Property(property="amount", type="number", description="Amount of the bonus"),
+ * )
+ */
 class Bonus extends Model
 {
     use HasFactory;
