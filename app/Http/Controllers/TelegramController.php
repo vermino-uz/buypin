744a7f847c7499d6dbb403ea69a5c26a1ed0ddba
vermino-uz <shukurullayev.elbek@icloud.com>
2024-09-12 01:58:55 +0500

    message

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Telegram\Bot\Laravel\Facades\Telegram;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Telegram\Bot\FileUpload\InputFile;
 class TelegramController extends Controller
 {
     public function webhook(Request $request)
@@ -16,10 +17,17 @@ class TelegramController extends Controller
         if($text == '/start'){
             $bot->sendPhoto([
                 'chat_id' => $chat_id,
-                'photo' => \Telegram\Bot\FileUpload\InputFile::create('https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'),
+                'photo' => InputFile::create('https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'),
                 'caption' => 'Welcome to our bot!',
+                'reply_markup' => [
+                    'inline_keyboard' => [
+                        [
+                            ['text' => 'Google', 'url' => 'https://www.google.com'],
+                            ['text' => 'Facebook', 'url' => 'https://www.facebook.com'],
+                        ],
+                    ],
+                ],
             ]);
-
         }
        
     }
