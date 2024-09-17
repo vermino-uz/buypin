19a67ddebaf1ea755e96706e7e91c58b2058c4e8
vermino-uz <shukurullayev.elbek@icloud.com>
2024-09-17 22:31:41 +0500

    message

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\RequestController;
 use App\Models\Bonus;
 use App\Http\Controllers\PromoCodeController;
 
-Route::get('requests/{user_id}', [RequestController::class, 'index']);
+Route::get('requests/{user_id}', [RequestController::class, 'show']);
 Route::post('requests', [RequestController::class, 'store']);
 Route::apiResource('users', BotUserController::class);
 Route::apiResource('games', GameController::class);
