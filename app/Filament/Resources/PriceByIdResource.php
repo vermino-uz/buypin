4461299aa0215548a8a14df80b67f7f5595b0460
vermino-uz <shukurullayev.elbek@icloud.com>
2024-09-18 01:51:36 +0500

    message

--- app/Filament/Resources/PriceByIdResource.php
+++ app/Filament/Resources/PriceByIdResource.php
@@ -14,6 +14,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Tables\Columns\TextColumn;
 use App\Models\Game;
+use Filament\Forms\Components\Select;
 class PriceByIdResource extends Resource
 {
     protected static ?string $model = PriceById::class;
