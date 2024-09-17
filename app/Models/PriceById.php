1384e5cdee9b50e0d8e0cc715102760775442c4d
vermino-uz <shukurullayev.elbek@icloud.com>
2024-09-18 01:49:07 +0500

    message

--- app/Models/PriceById.php
+++ app/Models/PriceById.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class PriceById extends Model
 {
     use HasFactory;
+    protected $table = 'price_by_ids';
+    protected $fillable = ['game_id', 'amount', 'price', 'is_active'];
 }
