380d130d4090ff3dc1234d546feb9fdd2813e782
vermino-uz <shukurullayev.elbek@icloud.com>
2024-09-17 22:08:57 +0500

    message

--- app/Http/Controllers/PromoCodeController.php
+++ app/Http/Controllers/PromoCodeController.php
@@ -67,7 +67,17 @@ class PromoCodeController extends Controller
      *         @OA\JsonContent(
      *             type="object",
      *             properties={
-     *                 @OA\Property(property="error", type="Infuccient balance", description="Error message")
+     *                 @OA\Property(property="error", type="string", description="Error message")
+     *             }
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Resource not found",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             properties={
+     *                 @OA\Property(property="error", type="string", description="Error message")
      *             }
      *         )
      *     )
