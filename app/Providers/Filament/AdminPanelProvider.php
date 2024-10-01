a3a51249d4c35251b668b9e01461ef6d40f25c79
vermino-uz <shukurullayev.elbek@icloud.com>
2024-10-02 03:22:31 +0500

    message

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -40,7 +40,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                \App\Filament\Widgets\BlogPostsChart::class,
+                \App\Filament\Widgets\CustomWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
---
+++ resources/views/filament/widgets/custom-widget.blade.php
@@ -0,0 +1,12 @@
+<x-filament-widgets::widget>
+    <x-filament::card>
+        <div class="h-12 flex items-center space-x-4 rtl:space-x-reverse">
+            <h2 class="text-lg sm:text-xl font-bold tracking-tight">
+                {{ __('Custom Widget') }}
+            </h2>
+        </div>
+
+        {{-- Add your custom widget content here --}}
+        <p>This is a custom widget. Add your own content and functionality here.</p>
+    </x-filament::card>
+</x-filament-widgets::widget>
