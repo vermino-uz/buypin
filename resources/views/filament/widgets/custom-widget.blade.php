53735cae8dbbbafd543999388901532ae1cbf999
vermino-uz <shukurullayev.elbek@icloud.com>
2024-10-02 03:43:09 +0500

    message

--- resources/views/filament/widgets/custom-widget.blade.php
+++ resources/views/filament/widgets/custom-widget.blade.php
@@ -1,6 +1,6 @@
 <x-filament-widgets::widget>
     <x-filament::card>
-        <div class="flex h-10 justify-between items-center">
+        <div class="flex h-[200px] justify-between items-center">
 
 
 
