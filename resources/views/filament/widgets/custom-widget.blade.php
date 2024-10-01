d14c9ba14e537acf59cc9f44eb3f482826defdde
vermino-uz <shukurullayev.elbek@icloud.com>
2024-10-02 03:29:19 +0500

    message

--- resources/views/filament/widgets/custom-widget.blade.php
+++ resources/views/filament/widgets/custom-widget.blade.php
@@ -1,24 +1,26 @@
-<x-filament-widgets::widget>
+<x-filament-widgets::widget class="fi-account-widget">
     <x-filament::card>
-        <div class="flex justify-between items-center">
-            <div class="flex flex-col">
-                <h2 class="text-lg sm:text-xl font-bold tracking-tight">
-                    {{ __('Custom Widget') }}
-                </h2>
-                <p class="text-sm text-gray-500">Admin panel by Vermino</p>
-            </div>
-            <div class="flex items-center">
-                <x-filament::button
-                    href="https://t.me/vermino"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    icon="heroicon-o-paper-airplane"
-                    class="self-center"
-                    color="primary"
-                    style="background-color: #0088cc; border-color: #0088cc;"
-                >
-                    Telegram
-                </x-filament::button>
+        <div class="flex items-center gap-x-3">
+            <div class="flex-1 flex flex-col justify-between h-full">
+                <div>
+                    <h2 class="text-lg sm:text-xl font-bold tracking-tight">
+                        {{ __('Custom Widget') }}
+                    </h2>
+                    <p class="text-sm text-gray-500">Admin panel by Vermino</p>
+                </div>
+                <div class="mt-4">
+                    <x-filament::button
+                        href="https://t.me/vermino"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        icon="heroicon-o-paper-airplane"
+                        class="w-full justify-center"
+                        color="primary"
+                        style="background-color: #0088cc; border-color: #0088cc;"
+                    >
+                        Telegram
+                    </x-filament::button>
+                </div>
             </div>
         </div>
     </x-filament::card>
