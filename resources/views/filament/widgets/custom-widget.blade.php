a01b59ea3c43595aa8ac1e6cbdaf5b4001af000f
vermino-uz <shukurullayev.elbek@icloud.com>
2024-10-02 03:46:59 +0500

    message

--- resources/views/filament/widgets/custom-widget.blade.php
+++ resources/views/filament/widgets/custom-widget.blade.php
@@ -1,33 +1,21 @@
 <x-filament-widgets::widget>
-    <x-filament::card class="h-[500px]">
-        <div class="flex justify-between items-center h-full">
-
-
-
-            <div class="flex flex-col">
-                <h2 class="text-lg sm:text-xl font-bold tracking-tight">
-                    {{ __('Custom Widget') }}
-                </h2>
-                <p class="text-sm text-gray-500">Admin panel by Vermino</p>
+    <x-filament::card>
+        <div class="flex justify-between items-center space-x-4">
+            <div>
+                <h2 class="text-lg font-bold tracking-tight">{{ __('Vermino') }}</h2>
+                <p class="text-xs text-gray-500">Admin panel by Vermino</p>
             </div>
-
-
-
-            <div class="flex items-center">
-                <x-filament::button
-                    href="https://t.me/vermino"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    icon="heroicon-o-paper-airplane"
-                    class="self-center"
-                    color="primary"
-                    style="background-color: #0088cc; border-color: #0088cc;"
-                >
-                    Telegram
-                </x-filament::button>
-            </div>
-
-
+            <x-filament::button
+                href="https://t.me/vermino"
+                target="_blank"
+                rel="noopener noreferrer"
+                icon="heroicon-o-paper-airplane"
+                color="primary"
+                size="sm"
+                style="background-color: #0088cc; border-color: #0088cc;"
+            >
+                Telegram
+            </x-filament::button>
         </div>
     </x-filament::card>
 </x-filament-widgets::widget>
