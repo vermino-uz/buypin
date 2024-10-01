b0f08ffc147becc134f807dfcaf26cebfb232685
vermino-uz <shukurullayev.elbek@icloud.com>
2024-10-02 00:11:29 +0500

    message

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -4,13 +4,18 @@ namespace App\Filament\Widgets;
 
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
-
+use App\Models\User;
+use App\Models\Game;
+use App\Models\PromoCode;
+    
 class StatsOverview extends BaseWidget
 {
     protected function getStats(): array
     {
         return [
-            //
+            Stat::make('Total Users', User::count()),
+            Stat::make('Total Games', Game::count()),
+            Stat::make('Total Promo Codes', PromoCode::count()),
         ];
     }
 }
