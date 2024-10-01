36cf73d61c5916d3dcc8c80f60a390d1bcc03c57
vermino-uz <shukurullayev.elbek@icloud.com>
2024-10-02 03:25:29 +0500

    message

--- resources/views/filament/widgets/custom-widget.blade.php
+++ resources/views/filament/widgets/custom-widget.blade.php
@@ -7,14 +7,16 @@
         </div>
 
         {{-- Add your custom widget content here --}}
-        <p>Admin panel by Vermino</p>
-        <x-filament::button
-            href="https://t.me/vermino"
-            target="_blank"
-            rel="noopener noreferrer"
-            icon="heroicon-o-paper-airplane"
-        >
-            Telegram
-        </x-filament::button>
+        <div class="flex items-center space-x-4">
+            <p>Admin panel by Vermino</p>
+            <x-filament::button
+                href="https://t.me/vermino"
+                target="_blank"
+                rel="noopener noreferrer"
+                icon="heroicon-o-paper-airplane"
+            >
+                Telegram
+            </x-filament::button>
+        </div>
     </x-filament::card>
 </x-filament-widgets::widget>
